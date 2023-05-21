d5ef9f082e2162429585ab8e32d3b423a5080d1a
rahul parmar <79297758+rahulrock9897@users.noreply.github.com>
2023-05-21 14:56:28 +0530

    Add files via upload

---
+++ 03inputexample.py
@@ -0,0 +1,7 @@
+# input method example
+
+interviewer = input("enter interviewer name :- ")
+applicant = input("enter applicant name :- ")
+time = input("enter time of interview :- ")
+
+print(interviewer + " will interview " + applicant + " at " + time)
